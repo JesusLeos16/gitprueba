2a7d97a7599ae839183afaeccb388008688f73cb
JesusLeos16 <jelm060716@gmail.com>
2025-11-06 08:35:16 -0600

    Fortnite

--- gitprueba/Form1.cs
+++ gitprueba/Form1.cs
@@ -6,5 +6,10 @@ namespace gitprueba
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fortnite");
+        }
     }
 }
